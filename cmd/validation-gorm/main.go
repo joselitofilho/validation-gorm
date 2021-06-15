421252a62edc22d847eba74e1f3eba36ad2556eb
Joselito Viveiros Nogueira Filho <joselito.viveiros@hp.com>
2021-06-15 20:25:26 -0300

    chore: has one example.

--- cmd/validation-gorm/main.go
+++ cmd/validation-gorm/main.go
@@ -8,6 +8,19 @@ import (
 	"gorm.io/gorm"
 )
 
+type Brand struct {
+	gorm.Model
+	Name   string
+	Active bool
+}
+
+type Product struct {
+	gorm.Model
+	Description string
+	Active      bool
+	Brand       Brand `gorm:"foreignKey:ID"`
+}
+
 func main() {
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", os.Getenv("DBHOST"), os.Getenv("DBPORT"), os.Getenv("DBUSER"), os.Getenv("DBPASS"), os.Getenv("DBNAME"), os.Getenv("SSLMODE"))
 	fmt.Println("dsn:", dsn)
@@ -18,5 +31,10 @@ func main() {
 
 	fmt.Println("Connected. db.Error", db.Error)
 
-	// TODO: Enjoy!
+	db.AutoMigrate(&Brand{}, &Product{})
+
+	db.Create(&Product{
+		Active:      true,
+		Description: "product 1 desc",
+		Brand:       Brand{Name: "brand1", Active: true}})
 }
